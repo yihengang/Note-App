e42179006aecfca6258e436e2cc198d1b1b90899
unknown <casonyiheng@hotmail.com>
2023-04-07 15:57:54 +0100

    baseURL change

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,14 +1,15 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3005/notes";
+const baseUrl = "http://localhost:3001/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  const nonExisting = {
-    id: 10000,
-    content: "This note is not saved to server",
-    important: true,
-  };
-  return request.then((response) => response.data.concat(nonExisting));
+  // const nonExisting = {
+  //   id: 10000,
+  //   content: "This note is not saved to server",
+  //   important: true,
+  // };
+  // return request.then((response) => response.data.concat(nonExisting));
+  return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
