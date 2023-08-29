0aeca14ae322de646b7df9a98cd85edb8666985c
unknown <casonyiheng@hotmail.com>
2023-08-29 22:31:57 +0100

    Using Jest

--- src/components/NoteForm.test.js
+++ src/components/NoteForm.test.js
@@ -1,5 +1,5 @@
 import { screen, render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import NoteForm from "./NoteForm.js";
 
--- src/components/Togglable.test.js
+++ src/components/Togglable.test.js
@@ -1,7 +1,7 @@
 import Togglable from "./Togglable";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 
 describe("<Togglable />", () => {
   let container;
